87c4a32fc7d1352eca10e77f6288931b4844620b
Nilo Otaviano <nilotaviano@gmail.com>
2016-03-25 11:17:14 -0300

    Roberts edge detection implemented.

--- src/openCVProject/RobertsEdgeDetection/RobertsController.kt
+++ src/openCVProject/RobertsEdgeDetection/RobertsController.kt
@@ -3,57 +3,99 @@ package openCVProject.RobertsEdgeDetection
 import javafx.fxml.FXML
 import javafx.scene.control.Button
 import javafx.scene.control.ScrollPane
-import javafx.scene.control.Slider
+import javafx.scene.image.Image
 import javafx.scene.image.ImageView
 import openCVProject.*
 import org.opencv.core.Mat
-import org.opencv.core.Size
 import org.opencv.imgproc.Imgproc
 
 class RobertsController {
     @FXML var imageView: ImageView? = null
     @FXML var btnDetectEdge: Button? = null
-    @FXML var threshholdSlider: Slider? = null
     @FXML var imgScroll: ScrollPane? = null
 
     private var grayScale: Mat? = null
-    private var edges: Mat? = null
 
-    private var processedImage: Mat? = null
-    private var lastThreshholdValue: Double = -1.0
+    var edgesImage: Image? = null
+    var horizontalEdgesImage: Image? = null
+    var verticalEdgesImage: Image? = null
 
     fun initialize() {
         imageView?.image = Context.image
         UIUtils.setZoomScroll(imageView!!, imgScroll!!)
 
         grayScale = Mat()
-        edges = Mat()
     }
 
     fun detectEdges() {
-        if (lastThreshholdValue != threshholdSlider?.value) {
-            processedImage = ImageUtils.imageToMat(Context.image!!)
-            processedImage = this.doCanny(processedImage!!)
-            lastThreshholdValue = threshholdSlider!!.value
+        if (edgesImage == null) {
+            edgesImage = doDetectEdges()
         }
-        imageView?.image = ImageUtils.mat2Image(processedImage!!)
+        imageView?.image = edgesImage
+    }
+
+    fun detectHorizontalEdges() {
+        if (horizontalEdgesImage == null) {
+            horizontalEdgesImage = doDetectEdges(detectHorizontal = true, detectVertical = false)
+        }
+        imageView?.image = horizontalEdgesImage
+    }
+
+    fun detectVerticalEdges() {
+        if (verticalEdgesImage == null) {
+            verticalEdgesImage = doDetectEdges(detectHorizontal = false, detectVertical = true)
+        }
+        imageView?.image = verticalEdgesImage
+    }
+
+    private fun doDetectEdges(detectHorizontal: Boolean = true, detectVertical: Boolean = true): Image {
+        var processedImage = ImageUtils.imageToMat(Context.image!!)
+        processedImage = this.doRoberts(processedImage, detectHorizontal, detectVertical)
+        return ImageUtils.mat2Image(processedImage)
     }
 
     fun restoreImage() {
         imageView?.image = Context.image
     }
 
-    private fun doCanny(frame: Mat): Mat {
+    private fun doRoberts(frame: Mat, detectHorizontal: Boolean, detectVertical: Boolean): Mat {
         Imgproc.cvtColor(frame, grayScale, Imgproc.COLOR_BGR2GRAY)
 
-        Imgproc.blur(grayScale, edges, Size(2.0, 2.0))
+        val edges = Mat(grayScale!!.size(), grayScale!!.type())
 
-        Imgproc.Canny(edges, edges, this.threshholdSlider!!.value, this.threshholdSlider!!.value * 3)
+        val robertsX = arrayOf(
+                intArrayOf(+1, 0),
+                intArrayOf(0, -1)
+        )
 
-        val result = Mat()
-        frame.copyTo(result, edges)
+        val robertsY = arrayOf(
+                intArrayOf(0, +1),
+                intArrayOf(-1, 0)
+        )
 
-        return result
+        for (row in 0..grayScale!!.rows() - 2) {
+            for (col in 0..grayScale!!.cols() - 2) {
+                val pixelX =
+                        if (detectHorizontal) {
+                            (robertsX[0][0] * grayScale!!.get(row, col).first()) + (robertsX[0][1] * grayScale!!.get(row, col + 1).first()) +
+                                    (robertsX[1][0] * grayScale!!.get(row + 1, col).first()) + (robertsX[1][1] * grayScale!!.get(row + 1, col + 1).first())
+                        } else {
+                            0.0
+                        }
+
+                val pixelY = if (detectVertical) {
+                    (robertsY[0][0] * grayScale!!.get(row, col).first()) + (robertsY[0][1] * grayScale!!.get(row, col + 1).first()) +
+                            (robertsY[1][0] * grayScale!!.get(row + 1, col).first()) + (robertsY[1][1] * grayScale!!.get(row + 1, col + 1).first())
+                } else {
+                    0.0
+                }
+
+                val pixel = (Math.sqrt((pixelX * pixelX) + (pixelY * pixelY)))
+                edges.put(row, col, pixel)
+            }
+        }
+
+        return edges
     }
 
     fun returnToPreviousState() {
